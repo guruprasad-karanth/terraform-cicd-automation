9785de2055d28329f936dcfbc8f69d7f0869d7c0
Guruprasad <guru@Mac.lan>
2026-06-08 22:08:48 +0530

    add GitHub Actions CI/CD pipeline

---
+++ terraform/ec2.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "ec2" {
+  name   = "${var.project_name}-ec2-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "${var.project_name}-ec2-sg" }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "${var.project_name}-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "app" {
+  ami                         = "ami-0c02fb55956c7d316"
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_1.id
+  vpc_security_group_ids      = [aws_security_group.ec2.id]
+  key_name                    = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y docker
+    systemctl start docker
+    systemctl enable docker
+    usermod -aG docker ec2-user
+  EOF
+
+  tags = { Name = "${var.project_name}-ec2" }
+}
